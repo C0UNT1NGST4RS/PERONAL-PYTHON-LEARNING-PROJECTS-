9ea5c56b5e1fd8277a7c6c5c611613036c4d85d1
el3ktraz <86217941+el3ktraz@users.noreply.github.com>
2023-07-27 01:04:39 +0530

    ufff

--- Mathfuntions.py
+++ Mathfuntions.py
@@ -1,2 +1,21 @@
 #this is a comment 
 print("hellow world")
+
+x =1
+y =1.0
+z =1j
+
+
+
+#convert from int to float
+
+a = float(x)
+print(a)
+
+c=complex(y)
+print(c)
+
+x=20  
+def my_function(x): 
+    return x + 5 
+print(x)
