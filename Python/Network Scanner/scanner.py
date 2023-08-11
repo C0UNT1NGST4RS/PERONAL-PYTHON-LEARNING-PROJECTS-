d95a6dc42fbac4dbfbf00f5e27a26eec041dd3a9
el3ktraz <86217941+el3ktraz@users.noreply.github.com>
2023-08-11 11:13:20 +0530

    minor changes

--- Python/Network Scanner/scanner.py
+++ Python/Network Scanner/scanner.py
@@ -1,6 +1,6 @@
 from socket import *
 import time 
-import tqdm
+from tqdm import tqdm
 
 
 starttime =time.time()
@@ -9,19 +9,21 @@ if __name__ == '__main__':
     target = input("enter the Host to scan:")
     t_ip = gethostbyname(target) 
     print("Strating scan on host :", t_ip)
+    
+    port_range = range(1,100)
+    progress_bar = tqdm(port_range, unit='port', desc='Scanning')
 
     for i in range(1,100):
         s = socket(AF_INET , SOCK_STREAM)
         conn = s.connect_ex((t_ip,i))
-        port_range = range(1,100)
-        progress_bar = tqdm(port_range, unit='port', desc='Scanning')
+        
    
         for i in progress_bar:
             s = socket(AF_INET, SOCK_STREAM)
 
-        if conn ==0:
-            print ('Port %d: OPEN' % (i,))
-        s.close()
+            if conn ==0:
+                print ('Port %d: OPEN' % (i,))
+            s.close()
 
 print("Scan completed")
 print("Total time taken :",time.time()-starttime)
