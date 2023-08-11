6d63ae2ba943d85f577463cfb5ccfc0c82208e45
el3ktraz <86217941+el3ktraz@users.noreply.github.com>
2023-08-11 10:59:38 +0530

    added a progressbar

--- Python/Network Scanner/scanner.py
+++ Python/Network Scanner/scanner.py
@@ -1,21 +1,28 @@
 from socket import *
 import time 
+import tqdm
+
 
 starttime =time.time()
 
 if __name__ == '__main__':
-    target = int(input("enter the Host to scan:"))
+    target = input("enter the Host to scan:")
     t_ip = gethostbyname(target) 
     print("Strating scan on host :", t_ip)
 
-    for i in range(1,500):
+    for i in range(1,100):
         s = socket(AF_INET , SOCK_STREAM)
         conn = s.connect_ex((t_ip,i))
+        port_range = range(1,100)
+        progress_bar = tqdm(port_range, unit='port', desc='Scanning')
+   
+        for i in progress_bar:
+            s = socket(AF_INET, SOCK_STREAM)
 
         if conn ==0:
-            print("PORT", i ,"is OPEN")
+            print ('Port %d: OPEN' % (i,))
         s.close()
-        
+
 print("Scan completed")
 print("Total time taken :",time.time()-starttime)
 
