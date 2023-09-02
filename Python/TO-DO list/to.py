3124fad2e9a829163f6c0a8a2ca2da3d3b4db7af
el3ktraz <86217941+el3ktraz@users.noreply.github.com>
2023-09-02 23:36:40 +0530

    completed code

--- Python/TO-DO list/to.py
+++ Python/TO-DO list/to.py
@@ -26,7 +26,19 @@ while True:
     print("2.RemoveTask")
     print("3.ListTask")
     print("4.Exit")
-
-    
+choice = input("Enter your choice (1/2/3/4): ")
+if choice == "1":
+    task = input("Enter the task: ")
+    add_task(task)
+elif choice == "2":
+    task = input("Enter the task to remove: ")
+    remove_task(task)
+elif choice == "3":
+    list_tasks()
+elif choice == "4":
+    print("Goodbye!")
+    exit
+else:
+    print("Invalid choice. Please select a valid option (1/2/3/4).")    
 
 
