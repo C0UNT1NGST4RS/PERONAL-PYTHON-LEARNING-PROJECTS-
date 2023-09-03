9ad1aa8b96d99ec4f0a62e068f358b4c4b460055
el3ktraz <86217941+el3ktraz@users.noreply.github.com>
2023-09-03 20:58:52 +0530

    some modifications

--- Python/TO-DO list/to.py
+++ Python/TO-DO list/to.py
@@ -1,32 +1,32 @@
-#to do list in cmd using python 
-
 tasks = []
 
 def addtask(task):
-    tasks.add(task)
-    print("Task {task} has been added! ")
-
+    tasks.append(task)
+    print("Task {task} has been added!")
 
 def removetask(task):
     if task in tasks:
         tasks.remove(task)
-        print("Task {task} has been removed")
-
-
-
+        print("Task {task} has been removed.")
+    else:
+        print(f"Task {task} not found.")
 
 def listtask():
-    for task in tasks:
-        print(task)
-
+    if tasks:
+        print("Tasks:")
+        for index, task in enumerate(tasks, start=1):
+            print("{index}. {task}")
+    else:
+        print("No tasks to display.")
 
 while True:
     print("\nOPTIONS:")
-    print("1.AddTask")
-    print("2.RemoveTask")
-    print("3.ListTask")
-    print("4.Exit")
+    print("1. Add Task")
+    print("2. Remove Task")
+    print("3. List Tasks")
+    print("4. Exit")
     choice = input("Enter your choice (1/2/3/4): ")
+    
     if choice == "1":
         task = input("Enter the task: ")
         addtask(task)
@@ -37,8 +37,9 @@ while True:
         listtask()
     elif choice == "4":
         print("Goodbye!")
-        exit
+        break  
     else:
-        print("Invalid choice. Please select a valid option (1/2/3/4).")    
+        print("Invalid choice. Please select a valid option (1/2/3/4).")
+    
 
 
